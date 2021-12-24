e0c5b5a9dbf76ac2352ee9c55eb5cdc862494e0b
Gitea <gitea@fake.local>
2021-12-24 19:26:52 +0530

    repeated

--- Welcome.java
+++ Welcome.java
@@ -5,6 +5,7 @@ System.out.println("Welcome to CI-CD pipeline");
 System.out.println("### This is revised ##");
 System.out.println("### Once again ##");
 System.out.println("### This to to trigger git-hook ##");
+System.out.println("### This to to trigger git-hook --Repeat ##");
 
 
 
