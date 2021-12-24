8f39888de157ee22227a312d688c6e6b0a720ef6
Gitea <gitea@fake.local>
2021-12-24 19:23:23 +0530

    trigger hook

--- Welcome.java
+++ Welcome.java
@@ -4,6 +4,8 @@ public static void main(String[] args){
 System.out.println("Welcome to CI-CD pipeline");
 System.out.println("### This is revised ##");
 System.out.println("### Once again ##");
+System.out.println("### This to to trigger git-hook ##");
+
 
 
 }
